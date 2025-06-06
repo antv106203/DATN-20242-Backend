8eb861d9cab1192ecce21fcefbd8c9db71a61bae
unknown <an106203h@gmail.com>
2025-06-06 23:21:22 +0700

    phan quyen

--- routes/deviceRoutes.js
+++ routes/deviceRoutes.js
@@ -2,11 +2,11 @@ const express = require('express');
 const deviceController = require("../controllers/deviceController");
 const isAuth = require("../middlewares/authMiddleware")
 const router = express.Router();
-router.post("/ListDevice.json", deviceController.getListDevice);
-router.post("/createNewDevice.json", deviceController.createNewDevice);
-router.put("/updateDevice.json", deviceController.updateDevice);
-router.get("/getUnregisteredDevices.json", deviceController.getUnregisteredDevices);
-router.post("/detailDevice.json", deviceController.getDetailDevice);
-router.post("/deleteDevice.json",isAuth(["GAURD"]), deviceController.deleteDevice);
+router.post("/ListDevice.json", isAuth(["ADMIN", "GAURD"]),deviceController.getListDevice);
+router.post("/createNewDevice.json", isAuth(["ADMIN", "GAURD"]),deviceController.createNewDevice);
+router.put("/updateDevice.json",isAuth(["ADMIN", "GAURD"]) ,deviceController.updateDevice);
+router.get("/getUnregisteredDevices.json",isAuth(["ADMIN", "GAURD"]) ,deviceController.getUnregisteredDevices);
+router.post("/detailDevice.json",isAuth(["ADMIN", "GAURD"]) ,deviceController.getDetailDevice);
+router.post("/deleteDevice.json",isAuth(["ADMIN"]), deviceController.deleteDevice);
 
 module.exports = router;
--- routes/fingerprintRoutes.js
+++ routes/fingerprintRoutes.js
@@ -4,13 +4,13 @@ const isAuth = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 router.post("/listFingerprint.json", isAuth(["ADMIN", "GAURD"]) ,fingerprintController.getListFingerprints);
-router.post("/disableFingerprint.json", fingerprintController.disableFingerprint);
-router.put("/enableFingerprint.json", fingerprintController.enableFingerprint);
-router.post("/requestCreateFingerprint.json", fingerprintController.requestCreateFingerprint);
-router.post("/createFingerprint.json", fingerprintController.createFingerprint);
-router.post("/getDetailFingerprint.json", fingerprintController.getDetailFingerprint);
-router.put("/updateFingerprint.json", fingerprintController.updateFingerprint);
-router.post("/deleteFingerprint.json", fingerprintController.deleteFingerprint);
+router.post("/disableFingerprint.json",isAuth(["ADMIN", "GAURD"]), fingerprintController.disableFingerprint);
+router.put("/enableFingerprint.json", isAuth(["ADMIN", "GAURD"]),fingerprintController.enableFingerprint);
+router.post("/requestCreateFingerprint.json", isAuth(["ADMIN", "GAURD"]),fingerprintController.requestCreateFingerprint);
+router.post("/createFingerprint.json",isAuth(["ADMIN", "GAURD"]) ,fingerprintController.createFingerprint);
+router.post("/getDetailFingerprint.json", isAuth(["ADMIN", "GAURD"]),isAuth(["ADMIN", "GAURD"]),fingerprintController.getDetailFingerprint);
+router.put("/updateFingerprint.json", isAuth(["ADMIN", "GAURD"]),fingerprintController.updateFingerprint);
+router.post("/deleteFingerprint.json", isAuth(["ADMIN", "GAURD"]),fingerprintController.deleteFingerprint);
 
 
 module.exports = router;
